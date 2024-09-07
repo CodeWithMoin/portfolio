a2ae7968442c9aa55a16bcd1d1f9d4845f1dbc46
Moinuddin Shaik <119571433+CodeWithMoin@users.noreply.github.com>
2024-09-07 15:13:17 +0000

    commit message: added images, updated package.json

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Moinuddin Shaik",
+  title: "AI ML Student & Content Creator",
+  email: "moinneditz7@gmail.com",
+  gitHub: "@codewithmoin",
+  instagram: "@moinneditz",
+  linkedIn: "moinuddin-shaik",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
